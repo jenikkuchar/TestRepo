b99e867d55b1898e109fb74cb54ec9454c6ddd39
jenikkuchar <91950308+jenikkuchar@users.noreply.github.com>
2025-03-03 08:06:55 +0100

    Update datapage.js

--- datapage.js
+++ datapage.js
@@ -1,1 +1,11 @@
+// Tento skript zpracovává data z proměnné dataPage
 
+console.log("Javascript byl načten");
+
+// Kontrola, zda proměnná dataPage existuje
+if (typeof dataPage !== 'undefined') {
+    // Vypíše obsah proměnné dataPage do konzole
+    console.log("Obsah dataPage: " + JSON.stringify(dataPage));
+} else {
+    console.error("Proměnná dataPage není definována!");
+}
